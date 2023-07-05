4886d4254cbe3a4bb2373fc8bb75c5daa6bf251e
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2023-07-05 15:25:23 -0300

    removed test code

--- src/app/controller/chunkController.js
+++ src/app/controller/chunkController.js
@@ -21,7 +21,6 @@ export const getChunk = async (req, res) => {
 		}(select null::uuid, null         , null::uuid           , null             , null::uuid         , null::uuid           , null           , t_u.local           , t_u.piece_of_clothing_type            from type_use t_u);`;
 	try {
 		const { rows } = await dbQuery.query(query);
-		log('getChunk', { rows });
 		const result = rows.reduce(
 			(previousChunk, currentRow) => {
 				let object;
@@ -60,21 +59,6 @@ export const getChunk = async (req, res) => {
 			chunkTableList.reduce((previousObject, currentItem) => ({ ...previousObject, [currentItem.modelName]: [] }), {})
 		);
 		return res.status(status.success).send(result);
-	} catch (error) {
-		return buildError(log, 'getLocal', error, res);
-	}
-};
-
-export const test = async (req, res) => {
-	log('test');
-	const query = `${''
-		} select l.id l_id , l.name l_name, null::uuid p_o_c_t_id, null p_o_c_t_name, null::uuid p_o_c_id, null::uuid p_o_c_type, null p_o_c_name, null::uuid t_u_local, null::uuid t_u_piece_of_clothing_type from local l union${''
-		}(select null::uuid, null         , p_o_c_t.id           , p_o_c_t.name     , null::uuid         , null::uuid           , null           , null::uuid          , null::uuid                            from piece_of_clothing_type p_o_c_t) union${''
-		}(select null::uuid, null         , null::uuid           , null             , p_o_c.id           , p_o_c.type           , p_o_c.name     , null::uuid          , null::uuid                            from piece_of_clothing p_o_c) union${''
-		}(select null::uuid, null         , null::uuid           , null             , null::uuid         , null::uuid           , null           , t_u.local           , t_u.piece_of_clothing_type            from type_use t_u);`;
-	try {
-		const { rows } = await dbQuery.query(query);
-		return res.status(status.success).send(rows);
 	} catch (error) {
 		return buildError(log, 'getLocal', error, res);
 	}
--- src/app/route/chunkRoute.js
+++ src/app/route/chunkRoute.js
@@ -2,11 +2,10 @@ import express from 'express';
 
 import verifyAuth from '../middleware/verifyAuth';
 
-import { getChunk, test } from '../controller/chunkController';
+import { getChunk } from '../controller/chunkController';
 
 const router = express.Router();
 
 router.get('/', verifyAuth, getChunk);
-router.get('/test', verifyAuth, test);
 
 export default router;
