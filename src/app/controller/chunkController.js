2b8d52483cee270ce53725ee4a53329df051fd26
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2023-07-05 14:57:34 -0300

    test fix

--- src/app/controller/chunkController.js
+++ src/app/controller/chunkController.js
@@ -21,30 +21,31 @@ export const getChunk = async (req, res) => {
 		}(select null::uuid, null         , null::uuid           , null             , null::uuid         , null::uuid           , null           , t_u.local           , t_u.piece_of_clothing_type            from type_use t_u);`;
 	try {
 		const { rows } = await dbQuery.query(query);
+		log('getChunk', { rows });
 		const result = rows.reduce(
 			(previousChunk, currentRow) => {
 				let object;
 				let table;
-				if ((!currentRow['l_id']) || (currentRow['l_id'] === 'null')) {
+				if ((!currentRow['l_id']) || (currentRow['l_id'] !== 'null')) {
 					object = {
 						id: currentRow['l_id'],
 						name: currentRow['l_name'],
 					};
 					table = 'local';
-				} else if ((!currentRow['p_o_c_t_id']) || (currentRow['p_o_c_t_id'] === 'null')) {
+				} else if ((!currentRow['p_o_c_t_id']) || (currentRow['p_o_c_t_id'] !== 'null')) {
 					object = {
 						id: currentRow['p_o_c_t_id'],
 						name: currentRow['p_o_c_t_name'],
 					};
 					table = 'pieceOfClothingType';
-				} else if ((!currentRow['p_o_c_id']) || (currentRow['p_o_c_id'] === 'null')) {
+				} else if ((!currentRow['p_o_c_id']) || (currentRow['p_o_c_id'] !== 'null')) {
 					object = {
 						id: currentRow['p_o_c_id'],
 						type: currentRow['p_o_c_type'],
 						name: currentRow['p_o_c_name'],
 					};
 					table = 'pieceOfClothing';
-				} else if ((!currentRow['t_u_local']) || (currentRow['t_u_local'] === 'null')) {
+				} else if ((!currentRow['t_u_local']) || (currentRow['t_u_local'] !== 'null')) {
 					object = {
 						local: currentRow['t_u_local'],
 						type: currentRow['t_u_piece_of_clothing_type'],
@@ -65,7 +66,7 @@ export const getChunk = async (req, res) => {
 };
 
 export const test = async (req, res) => {
-	log('getChunk');
+	log('test');
 	const query = `${''
 		} select l.id l_id , l.name l_name, null::uuid p_o_c_t_id, null p_o_c_t_name, null::uuid p_o_c_id, null::uuid p_o_c_type, null p_o_c_name, null::uuid t_u_local, null::uuid t_u_piece_of_clothing_type from local l union${''
 		}(select null::uuid, null         , p_o_c_t.id           , p_o_c_t.name     , null::uuid         , null::uuid           , null           , null::uuid          , null::uuid                            from piece_of_clothing_type p_o_c_t) union${''
