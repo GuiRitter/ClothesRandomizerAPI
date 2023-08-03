8ce483e52858e9e56ecf4d4b47127a3236f86ae6
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2023-08-03 09:48:24 -0300

    added date time to use

--- src/app/controller/useController.js
+++ src/app/controller/useController.js
@@ -24,7 +24,7 @@ export const decrement = async (req, res) => {
 
 export const getUse = async (req, res) => {
 	let { local, pieceOfClothingType } = req.query;
-	const query = 'SELECT p_o_c.id, p_o_c.name, u.counter FROM piece_of_clothing p_o_c LEFT OUTER JOIN use u ON u.piece_of_clothing = p_o_c.id AND u.local = $1 WHERE p_o_c.type = $2 ORDER BY p_o_c.name';
+	const query = 'SELECT p_o_c.id, p_o_c.name, u.counter, u.last_date_time FROM piece_of_clothing p_o_c LEFT OUTER JOIN use u ON u.piece_of_clothing = p_o_c.id AND u.local = $1 WHERE p_o_c.type = $2 ORDER BY p_o_c.name';
 	log('getUse', { local, pieceOfClothingType });
 	try {
 		const { rows } = await dbQuery.query(query, [local, pieceOfClothingType]);
