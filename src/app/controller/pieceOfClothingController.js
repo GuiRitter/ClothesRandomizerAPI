70ab13dd802617ef0f30f75671631bda1e90aedf
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2023-08-17 14:09:02 -0300

    added delete (cascade) piece of clothing

--- src/app/controller/pieceOfClothingController.js
+++ src/app/controller/pieceOfClothingController.js
@@ -11,10 +11,22 @@ import { getLog } from '../util/log';
 const log = getLog('pieceOfClothingController');
 
 export const getPieceOfClothing = async (req, res) => {
-	const query = 'SELECT p_o_c.id, p_o_c.type, p_o_c_t.name as piece_of_clothing_type, p_o_c.name FROM piece_of_clothing p_o_c JOIN piece_of_clothing_type p_o_c_t ON p_o_c.type = p_o_c_t.id ORDER BY p_o_c_t.name, p_o_c.name;';
+	const query = 'SELECT p_o_c.id, p_o_c.type, p_o_c_t.name as piece_of_clothing_type, p_o_c.name, (SELECT COUNT(*) FROM use u WHERE u.piece_of_clothing = p_o_c.id) > 0 AS has_dependency FROM piece_of_clothing p_o_c JOIN piece_of_clothing_type p_o_c_t ON p_o_c.type = p_o_c_t.id ORDER BY p_o_c_t.name, p_o_c.name;';
 	log('getPieceOfClothing');
 	try {
 		const { rows } = await dbQuery.query(query);
+		return res.status(status.success).send(rows);
+	} catch (error) {
+		return buildError(log, 'getPieceOfClothing', error, res);
+	}
+};
+
+export const deleteCascadePieceOfClothing = async (req, res) => {
+	let { id } = req.body;
+	const query = 'CALL delete_cascade_piece_of_clothing($1);';
+	log('deleteCascadePieceOfClothing', { id });
+	try {
+		const { rows } = await dbQuery.query(query, [id]);
 		return res.status(status.success).send(rows);
 	} catch (error) {
 		return buildError(log, 'getPieceOfClothing', error, res);
--- src/app/route/pieceOfClothingRoute.js
+++ src/app/route/pieceOfClothingRoute.js
@@ -2,10 +2,11 @@ import express from 'express';
 
 import verifyAuth from '../middleware/verifyAuth';
 
-import { getPieceOfClothing } from '../controller/pieceOfClothingController';
+import { deleteCascadePieceOfClothing, getPieceOfClothing } from '../controller/pieceOfClothingController';
 
 const router = express.Router();
 
+router.delete('/', verifyAuth, deleteCascadePieceOfClothing);
 router.get('/list', verifyAuth, getPieceOfClothing);
 
 export default router;
